85f3f4daa17abf79657d1d66e7debc6e768049e2
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-12 21:32:16 +0530

    First Commit

---
+++ Practice/src/GFG.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG 
+{ 
+	public static void main(String args[]) 
+    {
+		int i = 0, j = 9; 
+        do { 
+            i++; 
+            if (j-- < i++) { 
+                break; 
+            } 
+        } while (i < 5); 
+          System.out.println(i + "" + j); 
+    }
+}  
